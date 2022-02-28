71060c6fbcef521706db5602d5289e359cb5ec11
dulkifal <dulifilibacker@gmail.com>
2022-02-28 08:58:21 +0530

    db mongo

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 const connection = {}
-
-let prod=process.env.NODE_ENV === 'production'
+ 
 
  async function dbConnect() {
-    
   if(connection.isConnected) {
     return;
   }
-   const db = await mongoose.connect(prod?process.env.mongo123:process.env.mongo, {
+   const db = await mongoose.connect(process.env.mongo, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+
+   
 
   //connection.isConnected = db.connection[0].readyState;
   // console.log(connection.isConnected);
