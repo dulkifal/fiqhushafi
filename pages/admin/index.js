243726b9a683424a5d1e635e179fbc4a54bee76d
dulkifal <dulifilibacker@gmail.com>
2022-07-16 20:06:09 +0530

    password check

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -58,7 +58,10 @@ export default function adminPanel() {
   <button onClick={e => {
     e.preventDefault();
     localStorage.setItem('password', password.password);
-    setAdmin(true);
+    if(password.password === 'admin'){
+
+      setAdmin(true);
+    }
     
   }}
   type="submit">submit</button>
